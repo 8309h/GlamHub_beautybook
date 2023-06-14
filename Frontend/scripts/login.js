8eed066e757c6c82b802ca931d494b85a78e5304
JRaghu842 <jraghavendra842@gmail.com>
2023-06-15 00:57:30 +0530

    login and resister frontend

---
+++ Frontend/scripts/login.js
@@ -0,0 +1,46 @@
+let form = document.getElementById("formcontainer");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let payload = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  fetch(`http://localhost:4000/user/login`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.msg == "Login success") {
+        Swal.fire({
+          position: "centre",
+          icon: "success",
+          title: "Login Success",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTimeout(() => {
+          window.location.href = "lobby.html"; //----
+        }, 2500);
+      } else {
+        Swal.fire({
+          position: "centre",
+          icon: "error",
+          title: `${res.msg}`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+let googleBtn = document.getElementById("gbtn");
+
+googleBtn.addEventListener("click", () => {
+  // window.location.href="leaderboard.html"
+});
